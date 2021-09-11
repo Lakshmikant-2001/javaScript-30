8af23c6def03d5cf846776a9e949c9367586027e
lakshmikant <lakshmikant2512@gmail.com>
2021-09-11 10:37:44 +0530

    modify styles  --> js1

--- DrumKit-js1/main.js
+++ DrumKit-js1/main.js
@@ -1,11 +1,11 @@
 
 removePlayingClass = (e) =>{
-    // if(e.propertyName!=='transform') return;
     e.target.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll('.key');
 keys.forEach((key)=> key.addEventListener('transitionend',removePlayingClass));
+const body = document.querySelector('body');
 
 window.addEventListener('keydown',function(e){
     const dataKey = e.key.toUpperCase();       
@@ -15,5 +15,8 @@ window.addEventListener('keydown',function(e){
     audio.currentTime = 0;
     audio.play();
     keyDiv.classList.add('playing');
+    const span = keyDiv.querySelector('.sound');
+    const p = document.querySelector('.sound-name');
+    p.textContent=span.textContent.toUpperCase();
 })
 
