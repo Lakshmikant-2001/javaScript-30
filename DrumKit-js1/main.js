b484752344616e74ef0ad3e3341a37625eabef2f
lakshmikant <lakshmikant2512@gmail.com>
2021-09-11 15:25:19 +0530

    add click functionalily --> js1

--- DrumKit-js1/main.js
+++ DrumKit-js1/main.js
@@ -1,22 +1,40 @@
 
-removePlayingClass = (e) =>{
+removePlayingClass = (e) => {
     e.target.classList.remove('playing');
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach((key)=> key.addEventListener('transitionend',removePlayingClass));
-const body = document.querySelector('body');
-
-window.addEventListener('keydown',function(e){
-    const dataKey = e.key.toUpperCase();       
+playAudioKey = (e) => {
+    const dataKey = e.key.toUpperCase();
     const keyDiv = document.querySelector(`.key[data-key = "${dataKey}"]`);
     const audio = document.querySelector(`audio[data-key = "${dataKey}"]`);
-    if(!audio) return;
+    if (!audio) return;
     audio.currentTime = 0;
     audio.play();
     keyDiv.classList.add('playing');
     const span = keyDiv.querySelector('.sound');
     const p = document.querySelector('.sound-name');
-    p.textContent=span.textContent.toUpperCase();
-})
+    p.textContent = span.textContent.toUpperCase();
+}
 
+playAudioClick = (e) => {
+    const keyDiv = e.currentTarget;
+    const kbd = keyDiv.querySelector('kbd');
+    const dataKey = kbd.textContent;
+    const audio = document.querySelector(`audio[data-key = "${dataKey}"]`);
+    audio.currentTime = 0;
+    audio.play();
+    keyDiv.classList.add('playing');
+    const span = keyDiv.querySelector('.sound');
+    const p = document.querySelector('.sound-name');
+    p.textContent = span.textContent.toUpperCase();
+}
+
+const keys = document.querySelectorAll('.key');
+const body = document.querySelector('body');
+
+window.addEventListener('keydown', playAudioKey);
+
+keys.forEach((key) => {
+    key.addEventListener('transitionend', removePlayingClass);
+    key.addEventListener('click', playAudioClick);
+});
